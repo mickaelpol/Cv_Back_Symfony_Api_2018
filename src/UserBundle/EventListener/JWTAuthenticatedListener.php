c64516b9852d2c833d137018930493f579e82a94
Rdyx <tiranico.pro@gmail.com>
2018-10-08 08:30:43 +0200

    Added JWT Token + user_id in token creation and decode

---
+++ src/UserBundle/EventListener/JWTAuthenticatedListener.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace UserBundle\EventListener;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTAuthenticatedEvent;
+
+class JWTAuthenticatedListener {
+	/**
+	 * @param JWTAuthenticatedEvent $event
+	 *
+	 * @return void
+	 */
+	public function onJWTAuthenticated( JWTAuthenticatedEvent $event ) {
+		$token   = $event->getToken();
+		$payload = $event->getPayload();
+
+		$token->setAttribute( 'uuid', $payload['uuid'] );
+	}
+}
